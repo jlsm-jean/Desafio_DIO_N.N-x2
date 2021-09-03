ba4512064fa71233c7937c0df81e294e1aed1d59
Jean Leonardo Soares de Moraes <jean.jlsm2@gmail.com>
2021-09-03 15:59:31 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Desafio_DIO_N.N_x2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+
+            int n = Convert.ToInt32(Console.ReadLine()); 
+
+            for (int linha = 1; linha <= n; linha++)
+            {
+              Console.WriteLine(linha + " " + (linha * linha) + " " + (linha * linha * linha));
+
+              Console.WriteLine(linha + " " + (linha * linha + 1) + " " + (linha * linha * linha + 1));
+            }
+           
+           
+
+        }
+
+  
+    }
+}
